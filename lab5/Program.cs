718a4d644c85f49fe11de4fb842ced4e35062a3c
unknown <nizarrasyiid1221@gmail.com>
2024-03-23 08:29:51 +0700

    menambahkan class SimpleDatabase

--- lab5/Program.cs
+++ lab5/Program.cs
@@ -11,10 +11,39 @@ internal class Program
             return a + b + c;
         }
     }
+    public class SimpleDataBase<T>
+    {
+        private List<T> storedData { get;set; }
+        private List<DateTime> inputDates { get; set; }
 
-     static void Main(string[] args)
+        public SimpleDataBase()
+        {
+            storedData = new List<T>();
+            inputDates = new List<DateTime>();
+        }
+        public void AddNewData(T List)
+        {
+            storedData.Add(List);
+            inputDates.Add(DateTime.Now);
+        }
+        public void PrintAllData()
+        {
+            for (int i = 0; i < storedData.Count; i++)
+            {
+                Console.WriteLine("Data " + i + " berisi : " + storedData[i] + " yang disimpan pada waktu " + inputDates[i]);
+            }
+        }
+    }
+
+    static void Main(string[] args)
     {
         Penjumlahan<int> JumlahTigaAngka = new Penjumlahan<int>();
         Console.WriteLine(JumlahTigaAngka.JumlahTigaAngka(13, 02, 22));
+        SimpleDataBase<int> Data = new SimpleDataBase<int>();
+        Data.AddNewData(13);
+        Data.AddNewData(02);
+        Data.AddNewData(22);
+        Data.PrintAllData();
+        Console.ReadLine();
     }
 }
