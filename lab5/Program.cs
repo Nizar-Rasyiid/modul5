538f67c7b4c7617c7b60dc0b1af4a0a9baab9c21
unknown <nizarrasyiid1221@gmail.com>
2024-03-23 08:12:56 +0700

    implementasi-generic-method

--- lab5/Program.cs
+++ lab5/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Numerics;
+
+
+internal class Program
+{
+    public class Penjumlahan<T>
+    {
+
+        public T JumlahTigaAngka<T>(T a, T b, T c) where T : IAdditionOperators<T, T, T>
+        {
+            return a + b + c;
+        }
+    }
+
+     static void Main(string[] args)
+    {
+        Penjumlahan<int> JumlahTigaAngka = new Penjumlahan<int>();
+        Console.WriteLine(JumlahTigaAngka.JumlahTigaAngka(13, 02, 22));
+    }
+}
